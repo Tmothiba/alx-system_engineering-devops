9f98f403a74afc918334f7765de741c9857b4537
Tmothiba <mothibatshwarelo99@gmail.com>
2024-07-03 22:15:33 +0000

    0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # Create a file if it doesn't exist, and enter a character string.
 file {'/tmp/school':
-ensure  => present,
+ensure  => file,
 mode    => '0744',
 owner   => 'www-data',
 group   => 'www-data',
-content => 'I love Puppet',
+content => "I love Puppet\n",
 }
