2273ec0e567a57df6a6c5c953ece80cdbccbf388
Tmothiba <mothibatshwarelo99@gmail.com>
2024-07-29 21:03:10 +0000

    0-gather_data_from_an_API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,27 +2,32 @@
 """For a given employee ID, returns information about
 their TODO list progress"""
 
-import requests
-import sys
+from requests import get 
+from sys import argv
 
 if __name__ == "__main__":
-    # Base URL for the JSONPlaceholder API
-    url = "https://jsonplaceholder.typicode.com/"
+    response = get('https://jsonplaceholder.typicode.com/todos/')
+    data = response.json()
+    completed = 0
+    total = 0
+    tasks = []
+    response2 = get('https://jsonplaceholder.typicode.com/users')
+    data2 = response2.json()
 
-    # Get the employee information using the provided employee ID
-    employee_id = sys.argv[1]
-    user = requests.get(url + "users/{}".format(employee_id)).json()
+    for i in data2:
+        if i.get('id') == int(argv[1]):
+            employee = i.get('name')
 
-    # Get the to-do list for the employee using the provided employee ID
-    params = {"userId": employee_id}
-    todos = requests.get(url + "todos", params).json()
+    for i in data:
+        if i.get('userId') == int(argv[1]):
+            total += 1
 
-    # Filter completed tasks and count them
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
+            if i.get('completed') is True:
+                completed += 1
+                tasks.append(i.get('title'))
 
-    # Print the employee's name and the number of completed tasks
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
+    print("Employee {} is done with tasks({}/{}):".format(employee, completed,
+                                                          total))
 
-    # Print the completed tasks one by one with indentation
-    [print("\t {}".format(complete)) for complete in completed]
+    for i in tasks:
+        print("\t {}".format(i))
