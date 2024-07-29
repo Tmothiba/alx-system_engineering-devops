31d29989e5fae6ec3d3f354fe5a910dc2ec593ae
Tmothiba <mothibatshwarelo99@gmail.com>
2024-07-29 21:53:37 +0000

    Dictionary of list of dictionaries

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,17 +5,14 @@ their TODO list progress"""
 import requests
 import sys
 
-
 if __name__ == "__main__":
     # Base URL for the JSONPlaceholder API
     url = "https://jsonplaceholder.typicode.com/"
-
     # Get the employee information using the provided employee ID
     employee_id = sys.argv[1]
     user_response = requests.get(url + "users/{}".format(employee_id))
 
     user = user_response.json()
-
     # Get the to-do list for the employee using the provided employee ID
     params = {"userId": employee_id}
     response_todos = requests.get(url + "todos", params=params)
@@ -27,6 +24,5 @@ if __name__ == "__main__":
             completed.append(todo.get("title"))
 
     print("employee {} is done with tasks({}/{})".format(user.get("name"), len(completed), len(todos)))
-
     for complete in completed:
         print("\t {}".format(complete))
